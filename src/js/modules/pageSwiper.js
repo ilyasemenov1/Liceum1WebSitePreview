960b748dc3ff0de4503fd1266d7b89f6d98a185a
Ilya Semenov <sialogacc@gmail.com>
2023-02-26 19:59:10 +0300

    Add main swiper pagination

--- src/js/modules/pageSwiper.js
+++ src/js/modules/pageSwiper.js
@@ -12,9 +12,44 @@ var mainSwiper = new Swiper(".mainSwiper", {
     spaceBetween: 10,
     pagination: {
         clickable: true,
-        el: ".swiper-pagination"
+        el: ".swiper-pagination",
+        dynamicBullets: true
     },
     autoplay: {
         delay: 4000
-    },
+    }
 });
+
+mainSwiper.on("slideChange", () => {
+    slidesInit();
+});
+
+window.addEventListener("load", () => {
+    slidesInit();
+});
+
+const slidesInit = () => {
+    let swiperPagimationAdaptivity = new SwiperPagimationAdaptivity(mainSwiper.activeIndex);
+    swiperPagimationAdaptivity.crearClasses();
+    swiperPagimationAdaptivity.initClasses();
+}
+
+class SwiperPagimationAdaptivity {
+    constructor(slideNum) {
+        this.slides = document.querySelectorAll(".mainSwiper_wrapper>div");
+        this.container = document.querySelector(".mainSwiper_pagination");
+        this.slideNum = slideNum;
+    }
+
+    initClasses() {
+        if (this.slideNum == 0) {this.container.classList.add("first")}
+        if (this.slideNum == 1) {this.container.classList.add("second")}
+        if (this.slideNum == this.slides.length - 2) {this.container.classList.add("pre-last")}
+        if (this.slideNum == this.slides.length - 1) {this.container.classList.add("last")}
+        if (this.slides.length < 5) {this.container.classList.add(`length-${this.slides.length}`)}
+    }
+
+    crearClasses() {
+        this.container.classList.remove("first", "second", "pre-last", "last");
+    }
+}
