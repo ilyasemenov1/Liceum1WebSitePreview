5048fb38883b1926b1b10bb03fbd52ae8e9f1c43
Ilya Semenov <sialogacc@gmail.com>
2023-07-12 15:30:08 +0300

    Add images lazy load

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Проект редизайна сайта МОУ "Лицей №1"
+
+## Чтобы запустить проект, вам нужно:
+
+- Установить **[node.js](https://nodejs.org/en)**
+- Установить **[GraphicsMagic](http://www.graphicsmagick.org/INSTALL-windows.html#installing-using-installer-package)**
+- Установить **[ImageMagic](https://imagemagick.org/index.php)**
+- Ввести `npm i` и `npm run dev` в терминал
+
+*Версия проекта: `0.2.1`* 
--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,6 +1,8 @@
 
 import webp from "gulp-webp";
 import imagemin from "gulp-imagemin";
+import rename from "gulp-rename";
+import gm from "gulp-gm";
 
 let images = () => {
     return app.gulp.src(app.path.src.images)
@@ -21,6 +23,16 @@ let images = () => {
         interlaced: true,
         optimizationLevel: 3
     }))
+    .pipe(app.gulp.dest(app.path.build.images))
+    .pipe(app.gulp.src(app.path.src.images))
+    .pipe(gm(function (gmfile) {
+        gmfile.resize(20, 20);
+        return gmfile;
+      }))
+    .pipe(rename(function(opt) {
+        opt.basename = `${opt.basename}-low`
+        return opt;
+      }))
     .pipe(app.gulp.dest(app.path.build.images))
     .pipe(app.gulp.src(app.path.src.svg))
     .pipe(app.gulp.dest(app.path.build.images))
